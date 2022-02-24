ecf6c40356ef2e3d074417235e8c2326c618defb
PROVILLE Maximilien <maximilien.proville77@gmail.com>
2022-02-24 10:41:36 +0100

    24-02 10:41 --> State Settings and form progress

--- src/pages/EmployeeRegistration/EmployeeRegistration.js
+++ src/pages/EmployeeRegistration/EmployeeRegistration.js
@@ -4,7 +4,20 @@ import ReactDatePicker from "react-datepicker";
 import { useState } from "react";
 
 export const EmployeeRegistration = () => {
+  const [firstName, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [dateBirth, setDateBirth] = useState("");
   const [startDate, setStartDate] = useState(new Date());
+  //* Address States
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  //* Department State
+  const [department, setDepartment] = useState("");
+
+  console.log(startDate);
+
   return (
     <ReactDatePicker
       selected={startDate}
